0a852db2b6ed0562eacc2631338cc13bc8cc8a6f
Carlos <carlosaac232001@gmail.com>
2025-10-22 14:55:28 -0500

    chore: testing the new contains method

--- main.js
+++ main.js
@@ -17,3 +17,7 @@ console.log('Node at index: ', list.at(4));
 // Delete the last node
 list.pop();
 console.log('New last node: ', list.getTail());
+
+// Return true if the node data is the same as the value, not if there is not that data
+console.log(list.contains('parrot')); // false
+console.log(list.contains('cat')); // true
